fe25768986a2ef281e3bb8de11f84638cd3b0c2d
mobashshirhasan <mobashshir12@gmail.com>
2025-10-24 19:23:39 +0530

    updated

--- File Operations/main.py
+++ File Operations/main.py
@@ -1,5 +1,18 @@
-f = open('myfile.txt', 'r')
+# f = open('myfile.txt', 'r')
 
-text = f.read()
-print(text)
+# text = f.read()
+# print(text)
+# f.close()
+
+
+
+f = open('myfile3.txt', 'a')  #this wll open files for appending only
+f.write(' Hello, form India ')
 f.close()
+
+
+
+# f = open('myfile2.txt', 'w')    #this will open files for writing onluy and creates a new file if doesn't exist.
+# text = f.read()
+# print(text)
+# f.close()
