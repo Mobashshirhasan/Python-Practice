dee355039f9dec8cab642cb56dd4b92c9c367fe6
mobashshirhasan <mobashshir12@gmail.com>
2025-10-24 19:34:49 +0530

    updated

--- File Operations/main.py
+++ File Operations/main.py
@@ -6,9 +6,9 @@
 
 
 
-f = open('myfile3.txt', 'a')  #this wll open files for appending only
-f.write(' Hello, form India ')
-f.close()
+# f = open('myfile3.txt', 'a')  #this wll open files for appending only
+# f.write(' Hello, form India ')
+# f.close()
 
 
 
@@ -16,3 +16,17 @@ f.close()
 # text = f.read()
 # print(text)
 # f.close()
+
+
+
+
+
+# f = open('myfile2.txt', 'x')    #this will creates a files and gives an error if the file is already exists.
+# text = f.read()
+# print(text)
+# f.close()
+
+
+
+with open('myfile.txt', 'a') as f:
+    f.write("hey i am inside your file")
